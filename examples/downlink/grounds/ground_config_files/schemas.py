1bb1bea3ed2c1473f9d63df4c2d486eda8f62b8c
Matthew LeVine mlevine4 <mlevine4@stevens.edu>
2022-11-02 00:41:50 -0400

    Downlink Example Working
    Still work to do to post-process results, but downlink example case is operational with all correct messages. Scoreboard still needs some updates to display capacity of SSRs on each spacecraft.

--- examples/downlink/grounds/ground_config_files/schemas.py
+++ examples/downlink/grounds/ground_config_files/schemas.py
@@ -1,96 +1,110 @@
 # -*- coding: utf-8 -*-
 """
-Example script to specify object schemas for the FireSat test case.
+Example script to specify object schemas for the AWS Downlink test case.
 
 """
 
 from pydantic import BaseModel, Field, confloat
 from typing import Optional
 from datetime import datetime
-from enum import Enum
-
-
-class FireState(str, Enum):
-    undefined = "undefined"
-    started = "started"
-    detected = "detected"
-    reported = "reported"
-
-
-class FireStarted(BaseModel):
-    fireId: int = Field(..., description="Unique fire identifier.")
-    start: Optional[datetime] = Field(description="Time fire started.")
-    latitude: Optional[confloat(ge=-90, le=90)] = Field(
-        description="Latitude (deg) of fire location."
-    )
-    longitude: Optional[confloat(ge=-180, le=180)] = Field(
-        description="Longitude (deg) of fire location."
-    )
-
-
-class FireDetected(BaseModel):
-    fireId: int = Field(..., description="Unique fire identifier.")
-    detected: datetime = Field(..., description="Time fire detected.")
-    detected_by: str = Field(..., description="Satellite name that detected the fire.")
-
-
-class FireReported(BaseModel):
-    fireId: int = Field(..., description="Unique fire identifier.")
-    reported: datetime = Field(..., description="Time fire reported.")
-    reported_by: str = Field(
-        ..., description="Satellite name that sent the fire report."
-    )
-    reported_to: int = Field(
-        ..., description="Station id that received the fire report."
-    )
 
 
 class SatelliteReady(BaseModel):
     id: int = Field(..., description="Unique satellite identifier")
     name: str = Field(..., description="Satellite name for labeling")
+    ssr_capacity: float = Field(
+        ..., description="Capacity of onboard Solid State Recorder in Gigabytes"
+    )
     
     
 class SatelliteAllReady(BaseModel):
-    ready: str = Field("allReady", description="Indicates completion of SatelliteReady messages")
+    ready: str = Field(
+        "allReady", description="Indicates completion of SatelliteReady messages"
+    )
     
 
 class SatelliteStatus(BaseModel):
     id: int = Field(..., description="Unique satellite identifier")
-    name: str = Field(..., description="Satellite name for labeling.")
+    name: str = Field(..., description="Satellite name for labeling")
     latitude: confloat(ge=-90, le=90) = Field(
-        ..., description="Latitude (deg) of satellite subpoint location."
+        ..., description="Latitude (deg) of satellite subpoint location"
     )
     longitude: confloat(ge=-180, le=180) = Field(
-        ..., description="Longitude (deg) of satellite subpoint location."
+        ..., description="Longitude (deg) of satellite subpoint location"
     )
     altitude: float = Field(
         ..., description="Altitude (meters) of satellite above sea level"
     )
-    radius: float = Field(..., description="Radius of nadir pointing cone of vision")
-    pct_capacity_used: float = Field(..., description="Percent of solid state recorder capacity used")
+    capacity_used: float = Field(
+        ..., 
+        description="Fraction of solid state recorder capacity used"
+    )
     commRange: bool = Field(
         False, description="Boolean for if satellite is in ground stations view"
     )
-    groundId: Optional[int] = Field(...,description="Ground Station id in view (None if not in view)")
-    time: datetime = Field(..., description="Time in satellite reference frame")
+    groundId: Optional[int] = Field(
+        ..., description="Ground Station id in view (None if not in view)"
+    )
+    time: datetime = Field(
+        ..., description="Time in satellite reference frame"
+    )
 
 
 class GroundLocation(BaseModel):
-    groundId: int = Field(..., description="Unique ground station identifier.")
+    groundId: int = Field(..., description="Unique ground station identifier")
     latitude: confloat(ge=-90, le=90) = Field(
-        ..., description="Latitude (deg) of ground station."
+        ..., description="Latitude (deg) of ground station"
     )
     longitude: confloat(ge=-180, le=180) = Field(
-        ..., description="Longitude (deg) of ground station."
+        ..., description="Longitude (deg) of ground station"
     )
     elevAngle: float = Field(
-        ...,
-        description="Minimum elevation angle (deg) for satellite-ground communications",
+        ..., description="Minimum elevation angle (deg) for satellite-ground communications",
     )
     operational: bool = Field(
-        True, description="True, if this ground station is operational."
+        True, description="True, if this ground station is operational"
     )
     downlinkRate: float = Field(
+        ..., description="Downlink rate for this ground station in Megabytes per second"
+    )
+    costPerSecond: float = Field(
+        ..., description="Cost in $ per second for downlinks"
+    )
+    
+class LinkStart(BaseModel):
+    groundId: int = Field(..., description="Unique ground station identifier")
+    satId: int = Field(..., description="Unique satellite identifier that performed the downlink")
+    satName: str = Field(..., description="Name of satellite that performed the downlink")
+    linkId: int = Field(
+        ..., description="Unique downlink counter for the specific spacecraft that downlinked"
+    )
+    start: datetime = Field(
+        ..., description="Start time in ground network reference frame"
+    )
+    data: float = Field(
+        ..., description="Current amount of data on-board the satellite that needs to be offloaded"
+    )
+    
+class LinkCharge(BaseModel):
+    groundId: int = Field(..., description="Unique ground station identifier")
+    satId: int = Field(..., description="Unique satellite identifier that performed the downlink")
+    satName: str = Field(..., description="Name of satellite that performed the downlink")
+    linkId: int = Field(
+        ..., description="Unique downlink counter for the specific spacecraft that downlinked"
+    )
+    end: datetime = Field(
         ...,
-        description="Downlink rate for this ground station in Megabytes per second"
+        description="Time stamp for end of downlink when charge is sent"
+    )
+    duration: float = Field(
+        ..., description="Time duration of satellite in view for this downlink (s)"
+    )
+    dataOffload: float = Field(
+        ..., description="Amount of data offloaded based on linear downlink rates unique to each ground station (GB)"
+    )
+    downlinkCost: float = Field(
+        ..., description="Cost of data downlink based on per-second cost rates unique to each ground station"
+    )
+    cumulativeCosts: float = Field(
+        ..., description="Running total of ALL downlink costs for the entirety of the Test Case"
     )
