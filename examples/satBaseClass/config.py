660232031bc7f3c4f24075aac656b4cc6743895b
BrianC <98609737+bchell@users.noreply.github.com>
2023-02-09 11:39:35 -0500

    Fixed credentials
    Removed "SMCE_" from credential keywords

--- examples/satBaseClass/config.py
+++ examples/satBaseClass/config.py
@@ -9,7 +9,7 @@ import pandas as pd
 PARAMETERS = pd.Series(
     data={
         # Global parameters
-        "PREFIX": "satBaseClassTest",
+        "PREFIX": "BCTest",
         "SCALE": 60,
         "SCENARIO_START": datetime(2022, 11, 1, 7, 0, 0, tzinfo=timezone.utc).timestamp(),
         "SCENARIO_LENGTH": 1,
--- examples/satBaseClass/main_sat.py
+++ examples/satBaseClass/main_sat.py
@@ -5,7 +5,6 @@ from datetime import datetime, timezone, timedelta
 from dotenv import dotenv_values
 
 from skyfield.api import utc
-from keycloak import KeycloakOpenID
 from nost_tools.simulator import Mode
 from nost_tools.application_utils import ConnectionConfig, ShutDownObserver
 from nost_tools.managed_application import ManagedApplication
@@ -20,16 +19,10 @@ logging.basicConfig(level=logging.INFO)
 # name guard used to ensure script only executes if it is run as the __main__
 if __name__ == "__main__":
 
-    # keycloak_openid = KeycloakOpenID("http://localhost:7777/auth/",
-    #                                 client_id="solace",
-    #                                 realm_name="Master",
-    #                                 )
-
-
     # Note that these are loaded from a .env file in current working directory
     credentials = dotenv_values(".env")
-    HOST, PORT = credentials["SMCE_HOST"], int(credentials["SMCE_PORT"])
-    USERNAME, PASSWORD = credentials["SMCE_USERNAME"], credentials["SMCE_PASSWORD"]
+    HOST, PORT = credentials["HOST"], int(credentials["PORT"])
+    USERNAME, PASSWORD = credentials["USERNAME"], credentials["PASSWORD"]
     
     # set the client credentials
     config = ConnectionConfig(USERNAME, PASSWORD, HOST, PORT, True)
