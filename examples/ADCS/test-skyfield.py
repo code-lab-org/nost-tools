b05255887767025021be8f01d313b586fc4ee6f4
BrianC <98609737+bchell@users.noreply.github.com>
2023-04-19 18:28:27 -0400

    New ADCS test code

---
+++ examples/ADCS/test-skyfield.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 16:33:35 2023
+
+@author: brian
+"""
+
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from skyfield.api import load, Topos, EarthSatellite, wgs84
+from datetime import datetime, timezone, timedelta
+
+# Load the Earth's gravitational field model and the TLE data for the satellite
+ts = load.timescale()
+
+# GOES 18 tles
+line1 = '1 51850U 22021A   23103.96072472  .00000087  00000+0  00000+0 0  9995'
+line2 = '2 51850   0.0319  43.0117 0000436 329.8306  37.8882  1.00271539  4170'
+
+# Quetz sat
+# line1 = '1 29155U 06018A   23104.14393936  .00000024  00000+0  00000+0 0  9999'
+# line2 = '2 29155   1.3691  92.0151 0001511 167.1063  56.5698  1.00277598 31747'
+
+# TDRS 13
+# line1 = '1 42915U 17047A   23109.36291025 -.00000116  00000+0  00000+0 0  9990'
+# line2 = '2 42915   4.0741 337.0567 0017562 107.8120 241.4748  1.00272525 20769'
+
+# Create an EarthSatellite object from the TLE data
+satellite = EarthSatellite(line1, line2,'GOES 18')
+
+# Define the initial orientation of the satellite (pointing towards the negative z-axis)
+init_orientation = R.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
+
+# Define the nadir vector in the Earth-fixed frame
+nadir_vector = np.array([0, 0, -1])
+
+# Define the time range for the simulation
+start_time = datetime(2020, 4, 14, 7, 20, 0, tzinfo=timezone.utc)
+current_time = start_time
+end_time = datetime(2020, 4, 15, 8, 20, 0, tzinfo=timezone.utc)
+duration = (end_time - start_time).total_seconds()
+step = 30 # seconds
+num_steps = int(duration/step)
+
+
+# Initialize the satellite's position and orientation arrays
+positionsGeo = np.zeros((num_steps, 3))
+#positionsTopo = np.zeros((num_steps, 3))
+velocities = np.zeros((num_steps, 3))
+orientations = np.zeros((num_steps, 3))
+bxs = np.zeros((num_steps, 3))
+bys = np.zeros((num_steps, 3))
+bzs = np.zeros((num_steps, 3))
+
+# Define the rotation rate of the spacecraft in the body-fixed frame
+w = np.array([0, 0, 0.1])  # 0.1 rad/s about the body-fixed z-axis
+
+# Simulate the satellite's position and orientation over time
+for i in range(num_steps):
+    
+    time = ts.utc(current_time)
+    # inertial position 
+    geocentric = satellite.at(time).position.km    # position in GCRS/J2000
+    positionsGeo[i] = geocentric
+    # topocentric position
+    # topocentric = wgs84.geographic_position_of(satellite.at(time))
+    # positionsTopo = topocentric
+    
+    # Velocity 
+    velocity = satellite.at(time).velocity.km_per_s
+    velocities[i] = velocity
+    
+    # specific angular momentum
+    h = np.cross(geocentric, velocity)
+    
+    posMag = np.linalg.norm(geocentric)            # position magnitude
+    velMag = np.linalg.norm(velocity)            # velocity magnitude  
+    b_x = velocity/velMag                        # body x-axis along velocity vector
+    b_z = geocentric/posMag                        # body z-axis nadir pointing
+    b_y = np.cross(b_x,b_z)                      # body y-axis normal to orbital plane
+    
+    bxs[i] = b_x
+    bys[i] = b_y
+    bzs[i] = b_z
+    
+
+
+
+
+    
+    current_time = current_time + timedelta(0,step)
+
+    # Print 
+    #print(h)
+
--- examples/ADCS/testSatRecOrbit.py
+++ examples/ADCS/testSatRecOrbit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 13 16:33:35 2023
+Created on Wed Apr 19 15:15:07 2023
 
 @author: brian
 """
@@ -9,26 +9,32 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 from skyfield.api import load, Topos, EarthSatellite
 from datetime import datetime, timezone, timedelta
+from sgp4.api import Satrec, WGS84
 
 # Load the Earth's gravitational field model and the TLE data for the satellite
 ts = load.timescale()
-planets = load('de421.bsp')
 
-# GOES 18 tles
-# line1 = '1 51850U 22021A   23103.96072472  .00000087  00000+0  00000+0 0  9995'
-# line2 = '2 51850   0.0319  43.0117 0000436 329.8306  37.8882  1.00271539  4170'
+# create satellite from SatR
 
-# Quetz sat
-line1 = '1 29155U 06018A   23104.14393936  .00000024  00000+0  00000+0 0  9999'
-line2 = '2 29155   1.3691  92.0151 0001511 167.1063  56.5698  1.00277598 31747'
+satrec = Satrec()
+satrec.sgp4init(
+    WGS84,           # gravity model
+    'i',             # 'a' = old AFSPC mode, 'i' = improved mode
+    125,               # satnum: Satellite number
+    18441.785,       # epoch: days since 1949 December 31 00:00 UT
+    0,               # bstar: drag coefficient (/earth radii)
+    0,               # ndot: ballistic coefficient (revs/day)
+    0.0,             # nddot: second derivative of mean motion (revs/day^3)
+    0,               # ecco: eccentricity
+    0,               # argpo: argument of perigee (radians)
+    0,               # inclo: inclination (radians)
+    0,               # mo: mean anomaly (radians)
+    0.1,               # no_kozai: mean motion (radians/minute)
+    0                # nodeo: right ascension of ascending node (radians)
+)
 
-
-
-# Define the observer's location
-observer = Topos(latitude_degrees=0, longitude_degrees=0)
-
-# Create an EarthSatellite object from the TLE data
-satellite = EarthSatellite(line1, line2,'GOES 18')
+# Create an EarthSatellite object from satrec
+satellite = EarthSatellite.from_satrec(satrec,ts)
 
 # Define the initial orientation of the satellite (pointing towards the negative z-axis)
 init_orientation = R.from_euler('xyz', [0, 0, 180], degrees=True).as_quat()
@@ -36,7 +42,7 @@ init_orientation = R.from_euler('xyz', [0, 0, 180], degrees=True).as_quat()
 # Define the time range for the simulation
 start_time = datetime(2020, 4, 14, 7, 20, 0, tzinfo=timezone.utc)
 current_time = start_time
-end_time = datetime(2020, 4, 15, 8, 20, 0, tzinfo=timezone.utc)
+end_time = datetime(2020, 4, 14, 10, 20, 0, tzinfo=timezone.utc)
 duration = (end_time - start_time).total_seconds()
 step = 10 # seconds
 num_steps = int(duration/step)
@@ -77,12 +83,11 @@ for i in range(num_steps):
     else:
         rotation_axis = np.cross(np.array([0, 0, -1]), velocity_direction)
     rotation_angle = np.arccos(np.dot(np.array([0, 0, -1]), velocity_direction))
-    rotation_quat = R.from_rotvec(rotation_angle * rotation_axis).as_quat()
-    new_orientation = R.from_quat(rotation_quat) * R.from_quat(init_orientation)
-    orientations[i] = new_orientation.as_euler('xyz', degrees=True)
+    # rotation_quat = R.from_rotvec(rotation_angle * rotation_axis).as_quat()
+    # new_orientation = R.from_quat(rotation_quat) * R.from_quat(init_orientation)
+    # orientations[i] = new_orientation.as_euler('xyz', degrees=True)
     
     current_time = current_time + timedelta(0,step)
 
 # Print the final orientation of the satellite
 print('Final orientation:', orientations[-1])
-
