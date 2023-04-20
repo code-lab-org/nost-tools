96da6cc06ddeaf98b8d3532c69f72a5499ce79b8
BrianC <98609737+bchell@users.noreply.github.com>
2023-04-20 14:18:16 -0400

    Rotation testing

--- examples/ADCS/test-skyfield.py
+++ examples/ADCS/test-skyfield.py
@@ -28,11 +28,6 @@ line2 = '2 51850   0.0319  43.0117 0000436 329.8306  37.8882  1.00271539  4170'
 # Create an EarthSatellite object from the TLE data
 satellite = EarthSatellite(line1, line2,'GOES 18')
 
-# Define the initial orientation of the satellite (pointing towards the negative z-axis)
-init_orientation = R.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
-
-# Define the nadir vector in the Earth-fixed frame
-nadir_vector = np.array([0, 0, -1])
 
 # Define the time range for the simulation
 start_time = datetime(2020, 4, 14, 7, 20, 0, tzinfo=timezone.utc)
@@ -44,24 +39,34 @@ num_steps = int(duration/step)
 
 
 # Initialize the satellite's position and orientation arrays
-positionsGeo = np.zeros((num_steps, 3))
+positions= np.zeros((num_steps, 3))
 #positionsTopo = np.zeros((num_steps, 3))
 velocities = np.zeros((num_steps, 3))
 orientations = np.zeros((num_steps, 3))
 bxs = np.zeros((num_steps, 3))
 bys = np.zeros((num_steps, 3))
 bzs = np.zeros((num_steps, 3))
+orthcheck = np.zeros((num_steps))
 
-# Define the rotation rate of the spacecraft in the body-fixed frame
-w = np.array([0, 0, 0.1])  # 0.1 rad/s about the body-fixed z-axis
+# Define the initial orientation of the satellite (pointing towards the negative z-axis)
+init_orientation = R.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
+
+# Define the initial attitude in inertial frame
+init_position = satellite.at(ts.utc(start_time)).position.km
+init_velocity = satellite.at(ts.utc(start_time)).velocity.km_per_s
+posMag = np.linalg.norm(init_position)                        # position magnitude
+velMag = np.linalg.norm(init_velocity)                        # velocity magnitude  
+b_x = init_velocity/velMag                                    # body x-axis along velocity vector
+b_z = init_position/posMag                                    # body z-axis nadir pointing
+b_y = np.cross(b_x,b_z)                                       # body y-axis normal to orbital plane
 
 # Simulate the satellite's position and orientation over time
 for i in range(num_steps):
     
     time = ts.utc(current_time)
     # inertial position 
-    geocentric = satellite.at(time).position.km    # position in GCRS/J2000
-    positionsGeo[i] = geocentric
+    position = satellite.at(time).position.km    # position in GCRS/J2000
+    positions[i] = position
     # topocentric position
     # topocentric = wgs84.geographic_position_of(satellite.at(time))
     # positionsTopo = topocentric
@@ -71,17 +76,20 @@ for i in range(num_steps):
     velocities[i] = velocity
     
     # specific angular momentum
-    h = np.cross(geocentric, velocity)
+    h = np.cross(position, velocity)
     
-    posMag = np.linalg.norm(geocentric)            # position magnitude
+    posMag = np.linalg.norm(position)            # position magnitude
     velMag = np.linalg.norm(velocity)            # velocity magnitude  
     b_x = velocity/velMag                        # body x-axis along velocity vector
-    b_z = geocentric/posMag                        # body z-axis nadir pointing
+    b_z = position/posMag                        # body z-axis nadir pointing
     b_y = np.cross(b_x,b_z)                      # body y-axis normal to orbital plane
+    
+    orthcheck[i] = np.dot(b_x,b_z)
     
     bxs[i] = b_x
     bys[i] = b_y
     bzs[i] = b_z
+
     
 
 
