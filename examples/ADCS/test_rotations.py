75c864ceea3d70d2fd52f47febe827601c1896b1
BrianC <98609737+bchell@users.noreply.github.com>
2023-05-05 13:21:36 -0400

    ADCS testing

--- examples/ADCS/test_rotations.py
+++ examples/ADCS/test_rotations.py
@@ -13,35 +13,33 @@ import matplotlib.pyplot as plt
 
 
 # Define the position and velocity vectors
-r = np.array([
-            0,
-            0,
-            7189832.554930149
-])
-v = np.array([
-    7500,
-    0,
-    0
-])
+r = np.array([0, 0, 7189833])
+v = np.array([7500, 0, 0])
 
 # Define initial state of satellite
-cubeMass = 2                                                            # mass of single cubesat cube (kg)
-cubeLength = 0.1                                                        # length of single cubesat cube (m)
-# 6U cubesat inertia tensor                                    
-I = np.diag([cubeMass/12*((1*cubeLength)**2+(2*cubeLength)**2), 
-              cubeMass/12*((1*cubeLength)**2+(3*cubeLength)** 2), 
-              cubeMass/12*((2*cubeLength)**2+(3*cubeLength)**2)])   
-currentQuat = np.array([0.3826834323650898,                             # initial attitude quaternion (45 deg roll about x)
-                        0.0, 
-                        0.0, 
-                        0.9238795325112867])     
-w = np.array([0, 0, 0])                                                 # initial angular velocity in body frame
-euler = R.from_quat(currentQuat).as_euler('xyz',degrees=True)           # initial euler angles 
+# mass of single cubesat cube (kg)
+cubeMass = 2
+# length of single cubesat cube (m)
+cubeLength = 0.1
+# 6U cubesat inertia tensor
+# I = np.diag([cubeMass/12*((1*cubeLength)**2+(2*cubeLength)**2),
+#              cubeMass/12*((1*cubeLength)**2+(3*cubeLength) ** 2),
+#              cubeMass/12*((2*cubeLength)**2+(3*cubeLength)**2)])
+I = np.diag([1000, 500, 700])
+currentQuat = np.array([0,0,0,1])
+rot = R.from_euler('z', [-60], degrees=True)
+targetQuat = np.array([0.,	0.,	-0.5,	.866025403784438707])
+
+# initial angular velocity in body frame
+w = np.array([0, 0, 0])
+alpha = np.array([0, 0, 0])
+eulerRad = np.array([0, 0, 0])
+#euler = R.from_quat(currentQuat).as_euler('xyz', degrees=True)                           # initial euler angles
 
 # Define PID controller gains
-Kp = np.array([.01, 1, 1])         # proportional gain
-# Ki = np.array([0.1, 0.1, 0.1])   # integral gain
-Kd = np.array([0.0001, 0., 0.])   # derivative gain
+Kp = np.array([1000, 500, 70])         # proportional gain
+# Ki = np.array([0.1, 0.1, 0.1])       # integral gain
+Kd = np.array([2000, 100, 1400])       # derivative gain
 
 # Define reaction wheel specifications
 Iw = np.array([0.1, 0.1, 0.1])    # moment of inertia of each wheel
@@ -53,51 +51,60 @@ t_final = 60   # final time
 steps = int(t_final/dt)
 
 # Initialize arrays
-alpha_hist = np.zeros((steps,3))
-euler_hist = np.zeros((steps,3))
+alpha_hist = np.zeros((steps, 3))
+euler_hist = np.zeros((steps, 3))
 w_hist = np.zeros((steps, 3))
 q_hist = np.zeros((steps, 4))
 torque_hist = np.zeros((steps, 3))
 errorQuat = np.zeros(4)
 T_c = np.zeros(3)
 
-## setting up reference coordinate frame
+# setting up reference coordinate frame
 # Calculate the specific angular momentum vector
 h = np.cross(r, v)
 # Calculate the unit vectors for the reference x, y, and z axes
 y_r = h / np.linalg.norm(h)
-z_r = r / np.linalg.norm(r) 
+z_r = r / np.linalg.norm(r)
 x_r = np.cross(y_r, z_r)
 # Calculate the rotation matrix from the reference to the inertial frame (body to inertial?)
 R_bo = np.vstack((x_r, y_r, z_r)).T
 # Calculate the rotation matrix from the inertial to the body frame
 # R_ib = R_bo.T
 # Convert the rotation matrix to a quaternion
-targetQuat = R.from_matrix(R_bo).as_quat()
+# targetQuat = R.from_matrix(R_bo).as_quat()
+# targetQuat = np.array([0, -0.34202, 0, 0.93969])
 
 
 
 # Calculate error between current quat and desired quat
 def att_error(currentQuat, targetQuat):
-    
+
     qT = np.array([[targetQuat[3], targetQuat[2], -targetQuat[1], targetQuat[0]],
                   [-targetQuat[2], targetQuat[3], targetQuat[0], targetQuat[1]],
                   [targetQuat[1], -targetQuat[0], targetQuat[3], targetQuat[2]],
                   [-targetQuat[0], -targetQuat[1], -targetQuat[2], targetQuat[3]]])
-    qB = np.array([-currentQuat[0], -currentQuat[1], -currentQuat[2], currentQuat[3]])
-    errorQuat = np.matmul(qT,qB)
-    
+    qB = np.array([-currentQuat[0], -currentQuat[1], -
+                  currentQuat[2], currentQuat[3]])
+    errorQuat = np.matmul(qT, qB)
+
     return errorQuat
 
 # Calculate torque produced by reaction wheels (in body frame?)
+
+
 def control_torque(errorQuat, Kp, Kd, w):
-    
+
     T_c[0] = 2*Kp[0]*errorQuat[0]*errorQuat[3] + Kd[0]*w[0]
     T_c[1] = 2*Kp[0]*errorQuat[1]*errorQuat[3] + Kd[1]*w[1]
     T_c[2] = 2*Kp[0]*errorQuat[2]*errorQuat[3] + Kd[2]*w[2]
-    
+
     return T_c
 
+
+omegaPrime = np.array([[0, w[2], -w[1], w[0]],
+                       [-w[2], 0, w[0], w[1]],
+                       [w[1], -w[0], 0, w[2]],
+                       [-w[0], -w[1], -w[2], 0]])
 
 # Run simulation
 for i in range(steps):
@@ -107,25 +114,25 @@ for i in range(steps):
     # Calculate torque produced by reaction wheels
     T_c = control_torque(errorQuat, Kp, Kd, w)
 
-    # Update angular velocity, euler angles, and quaternion 
+    # Update angular velocity, euler angles, and quaternion
     alpha = np.matmul(np.linalg.inv(I), T_c)
-    w = alpha*dt                         
-    currentQuat = currentQuat + 0.5*np.array([w[0],w[1],w[2],0])*dt
+    w = w + alpha*dt
+    eulerRad = eulerRad + w*dt +0.5*alpha*dt**2
+    euler = np.degrees(eulerRad)
+    currentQuat = currentQuat + 0.5*np.array([w[0], w[1], w[2], 0])*dt
     # currentQuat = currentQuat / np.linalg.norm(currentQuat)
-    euler = R.from_quat(currentQuat).as_euler('xyz',degrees=True) 
+    # euler = R.from_quat(currentQuat).as_euler('zyx', degrees=True)
 
     # Store results
-    alpha_hist[i,:] = alpha
-    w_hist[i,:] = w
-    euler_hist[i,:] = euler
-    q_hist[i,:] = currentQuat
+    alpha_hist[i, :] = alpha
+    w_hist[i, :] = w
+    euler_hist[i, :] = euler
+    q_hist[i, :] = currentQuat
 
-    torque_hist[i,:] = T_c
-    
-plt.plot(euler_hist[:,0])
+    torque_hist[i, :] = T_c
+
+plt.plot(euler_hist[:, 2])
 plt.show
-    
-
 
 
 # print("Quaternion for Cesium", quat[0], ",", quat[1], ",", quat[2], ",", quat[3])
