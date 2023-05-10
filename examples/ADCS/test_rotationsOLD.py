bc846acb27778ce87df9e617f0283e838e59afab
BrianC <98609737+bchell@users.noreply.github.com>
2023-05-10 17:57:51 -0400

    Control torque testing

--- examples/ADCS/test_rotationsOLD.py
+++ examples/ADCS/test_rotationsOLD.py
@@ -7,11 +7,10 @@ Created on Tue Apr 25 18:10:44 2023
 
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-from scipy.integrate import ode
-# from skyfield.api import load, Topos, EarthSatellite, wgs84
-# from datetime import datetime, timezone, timedelta
+from skyfield.api import load, Topos, EarthSatellite, wgs84
+from datetime import datetime, timezone, timedelta
 import matplotlib.pyplot as plt
-# import skinematics as skin
+#import skinematics as skin
 
 
 # Define the position and velocity vectors
@@ -31,15 +30,10 @@ cubeLength = 0.1
 #from Wie
 #I = np.array([[1200, 100, -200],[100, 2200, 300],[-200, 300, 3100]])
 
-# from  Sidi book
 I = np.diag([1000, 500, 700])
 currentQuat = np.array([0,0,0,1])
-# rot = R.from_euler('zyx', [-60,-40,40], degrees=True)
-# targetQuat = (R.as_quat(rot))
-targetQuat = np.array([0.439032, -0.117638, -0.542817, 0.706231])
-Kp = np.array([1000, 500, 70])         # proportional gain
-# Ki = np.array([0.1, 0.1, 0.1])       # integral gain
-Kd = np.array([2000, 100, 1400])       # derivative gain
+rot = R.from_euler('z', [-60], degrees=True)
+targetQuat = np.array([0.,	0.,	-0.5,	.866025403784438707])
 
 # initial angular velocity in body frame
 w = np.array([0, 0, 0])
@@ -48,16 +42,16 @@ eulerRad = np.array([0, 0, 0])
 #euler = R.from_quat(currentQuat).as_euler('xyz', degrees=True)                           # initial euler angles
 
 # Define PID controller gains
-# Kp = np.array([1000, 500, 70])         # proportional gain
+Kp = np.array([1000, 500, 70])         # proportional gain
 # Ki = np.array([0.1, 0.1, 0.1])       # integral gain
-# Kd = np.array([2000, 100, 1400])       # derivative gain
+Kd = np.array([2000, 500, 10])       # derivative gain
 
 # Define reaction wheel specifications
 Iw = np.array([0.1, 0.1, 0.1])    # moment of inertia of each wheel
 max_torque = np.array([1, 1, 1])  # maximum torque each wheel can produce
 
 # Define simulation parameters
-dt = .1   # time step
+dt = 1   # time step
 t_final = 60   # final time
 steps = int(t_final/dt)
 
@@ -86,15 +80,51 @@ R_bo = np.vstack((x_r, y_r, z_r)).T
 # targetQuat = np.array([0, -0.34202, 0, 0.93969])
 
 
+def quaternion_multiply(Q0,Q1):
+    """
+    Multiplies two quaternions.
+ 
+    Input
+    :param Q0: A 4 element array containing the first quaternion (q01,q11,q21,q31) 
+    :param Q1: A 4 element array containing the second quaternion (q02,q12,q22,q32) 
+ 
+    Output
+    :return: A 4 element array containing the final quaternion (q03,q13,q23,q33) 
+ 
+    """
+    # Extract the values from Q0
+    x0 = Q0[0]
+    y0 = Q0[1]
+    z0 = Q0[2]
+    w0 = Q0[3]
+     
+    # Extract the values from Q1
+    x1 = Q1[0]
+    y1 = Q1[1]
+    z1 = Q1[2]
+    w1 = Q1[3]
+     
+    # Computer the product of the two quaternions, term by term
+    Q0Q1_w = w0 * w1 - x0 * x1 - y0 * y1 - z0 * z1
+    Q0Q1_x = w0 * x1 + x0 * w1 + y0 * z1 - z0 * y1
+    Q0Q1_y = w0 * y1 - x0 * z1 + y0 * w1 + z0 * x1
+    Q0Q1_z = w0 * z1 + x0 * y1 - y0 * x1 + z0 * w1
+     
+    # Create a 4 element array containing the final quaternion
+    final_quaternion = np.array([Q0Q1_w, Q0Q1_x, Q0Q1_y, Q0Q1_z])
+     
+    # Return a 4 element array containing the final quaternion (q02,q12,q22,q32) 
+    return final_quaternion
 
-# Calculate error between current quat and desired quat (Wie style)
+
+# Calculate error between current quat and desired quat 
 def att_error(currentQuat, targetQuat):
 
-    qT = np.array([[targetQuat[3], targetQuat[2], -targetQuat[1], -targetQuat[0]],
-                   [-targetQuat[2], targetQuat[3], targetQuat[0], -targetQuat[1]],
-                   [targetQuat[1], -targetQuat[0], targetQuat[3], -targetQuat[2]],
-                   [targetQuat[0], targetQuat[1], targetQuat[2], targetQuat[3]]])
-    qB = np.array([currentQuat[0], currentQuat[1], currentQuat[2], currentQuat[3]])
+    qT = np.array([[targetQuat[3], targetQuat[2], -targetQuat[1], targetQuat[0]],
+                  [-targetQuat[2], targetQuat[3], targetQuat[0], targetQuat[1]],
+                  [targetQuat[1], -targetQuat[0], targetQuat[3], targetQuat[2]],
+                  [-targetQuat[0], -targetQuat[1], -targetQuat[2], targetQuat[3]]])
+    qB = np.array([-currentQuat[0], -currentQuat[1], -currentQuat[2], currentQuat[3]])
     errorQuat = np.matmul(qT, qB)
 
     return errorQuat
@@ -105,30 +135,16 @@ def att_error(currentQuat, targetQuat):
 def control_torque(errorQuat, Kp, Kd, w):
 
     T_c[0] = 2*Kp[0]*errorQuat[0]*errorQuat[3] + Kd[0]*w[0]
-    T_c[1] = 2*Kp[1]*errorQuat[1]*errorQuat[3] + Kd[1]*w[1]
-    T_c[2] = 2*Kp[2]*errorQuat[2]*errorQuat[3] + Kd[2]*w[2]
+    T_c[1] = 2*Kp[0]*errorQuat[1]*errorQuat[3] + Kd[1]*w[1]
+    T_c[2] = 2*Kp[0]*errorQuat[2]*errorQuat[3] + Kd[2]*w[2]
 
     return T_c
 
-def taylorIntegrate(w, currentQuat, dt):
-    
-    D2 = (w[0]*dt)**2 + (w[1]**dt)**2 + (w[2]*dt)**2
-    R1 = 0.5*(w[0]*dt*currentQuat[3]-w[1]*dt*currentQuat[2]+w[2]*dt*currentQuat[1])
-    R2 = 0.5*(w[0]*dt*currentQuat[2]+w[1]*dt*currentQuat[3]-w[2]*dt*currentQuat[0])
-    R3 = 0.5*(-w[0]*dt*currentQuat[1]+w[1]*dt*currentQuat[0]+w[2]*dt*currentQuat[3])
-    R4 = 0.5*(-w[0]*dt*currentQuat[0]-w[1]*dt*currentQuat[1]-w[2]*dt*currentQuat[3])
-    Ri = np.array([R1, R2, R3, R4])
-    
-    currentQuat = currentQuat + Ri-D2*currentQuat-D2*Ri/3+D2**2*currentQuat/6
-    
-    return currentQuat
-    
 
 omegaPrime = np.array([[0, w[2], -w[1], w[0]],
                        [-w[2], 0, w[0], w[1]],
                        [w[1], -w[0], 0, w[2]],
-                       [-w[0], -w[1], -w[2], 0]]) 
-    
+                       [-w[0], -w[1], -w[2], 0]])
 
 # Run simulation
 for i in range(steps):
@@ -143,21 +159,27 @@ for i in range(steps):
     w = w + alpha*dt
     eulerRad = eulerRad + w*dt +0.5*alpha*dt**2
     euler = np.degrees(eulerRad)
-    currentQuat = taylorIntegrate(w, currentQuat, dt)
+    eulerRot = R.from_euler('xyz', np.array(eulerRad))
+    currentQuat = eulerRot.as_quat()
     
-    
-#     # from Sola
-#     # qwdt = [np.cos(np.linalg.norm(w)*dt/2)],[((w/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2))]
-#     # qwdt = np.array([np.cos(np.linalg.norm(w)*dt/2),((w[0]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2)),((w[1]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2)),((w[2]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2))])
-#     # currentQuat = quaternion_multiply(currentQuat, qwdt)
-    
-#     #qdot = 
-    
-#     currentQuat = currentQuat + 0.5*currentQuat*np.array([w[0], w[1], w[2], 0])*dt
-#     # currentQuat = currentQuat / np.linalg.norm(currentQuat)
-#     # euler = R.from_quat(currentQuat).as_euler('zyx', degrees=True)
+    # with scikit-kinematics
+    #currentQuat = skin.quat.Quaternion(currentQuat)
+    # currentQuat = skin.quat.calc_quat(w,currentQuat[:3],1,"sf")
+    # currentQuat = np.array([currentQuat[0,1],currentQuat[0,2],currentQuat[0,3],currentQuat[0,0]])
 
-#     # Store results
+    
+    # from Sola
+    # qwdt = [np.cos(np.linalg.norm(w)*dt/2)],[((w/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2))]
+    # qwdt = np.array([np.cos(np.linalg.norm(w)*dt/2),((w[0]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2)),((w[1]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2)),((w[2]/np.linalg.norm(w))*np.sin(np.linalg.norm(w)*dt/2))])
+    # currentQuat = quaternion_multiply(currentQuat, qwdt)
+    
+    #qdot = 
+    
+    currentQuat = currentQuat + 0.5*currentQuat*np.array([w[0], w[1], w[2], 0])*dt
+    # currentQuat = currentQuat / np.linalg.norm(currentQuat)
+    # euler = R.from_quat(currentQuat).as_euler('zyx', degrees=True)
+
+    # Store results
     alpha_hist[i, :] = alpha
     w_hist[i, :] = w
     euler_hist[i, :] = euler
