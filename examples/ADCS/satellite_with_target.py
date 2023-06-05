965fc90eeee777e1ebb40e8c56b4fe021519f31c
BrianC <98609737+bchell@users.noreply.github.com>
2023-06-05 16:51:01 -0400

    Revert "Attitude roll working"
    This reverts commit 1e2ad247061229fdd3befb71208ce6e9d2b22317.

--- examples/ADCS/satellite_with_target.py
+++ examples/ADCS/satellite_with_target.py
@@ -204,52 +204,13 @@ class Satellite(Entity):
         return isInRange, groundId
     
     # find target quaternion at culmination from ground location
-    def update_target_attitude(self, hoboken, t_start, t_end):
+    def update_target(self, hoboken, t_start, t_end):
        
-        t, events = satellite.find_events(hoboken, t0, t1, altitude_degrees=15.0)
+        t, events = ES.find_events(hoboken, t_start, t_end, altitude_degrees=30.0)
         eventZip = list(zip(t,events))
         df = pd.DataFrame(eventZip, columns = ["Time", "Event"])
-        culmTimes = df.loc[df["Event"]==1]
-        
-        geocentric = satellite.at(culmTimes.iloc[0]["Time"])
-        
-        culmTime = (culmTimes.iloc[0]["Time"]).utc_iso()
-        culmPos = geocentric.position.m
-        targetPos = hoboken.at(culmTimes.iloc[0]["Time"]).position.m
-        culmVel = geocentric.velocity.m_per_s
-        
-        h = np.cross(culmPos, culmVel)
-        # Calculate the unit vectors for the body x, y, and z axes
-        b_y = h / np.linalg.norm(h)
-        b_z = culmPos / np.linalg.norm(culmPos)
-        b_x0 = np.cross(b_y, b_z)
-        b_x = b_x0 / np.linalg.norm(b_x0)
-        
-        # Calculate the rotation matrix from the body to the inertial frame
-        R_bi = np.vstack((b_x, b_y, b_z)).T
-        
-        # Calculate the rotation matrix from the inertial to the body frame
-        R_ib = R_bi.T
-        
-        # Convert the rotation matrix to a quaternion
-        iQuat = R.from_matrix(R_bi).as_quat()
-        
-        print("iQuat", iQuat[0], ",", iQuat[1], ",", iQuat[2], ",", iQuat[3])
-        
-        # find roll angle between nadir vector and target
-        culmUnitVec = culmPos/np.linalg.norm(culmPos)
-        targetUnitVec = targetPos/np.linalg.norm(targetPos)
-        
-        direction = culmPos - targetPos
-        dirUnit = direction/np.linalg.norm(direction)
-        
-        rollAngle = np.arccos(np.dot(dirUnit, culmUnitVec))
-        
-        targetRot = R.from_matrix(R_bi)*R.from_euler('x',rollAngle)
-targetQuat = targetRot.as_quat()
-        
-        return targetQuat
-            
+        culmTimes = df.loc[df["Event"]==2]
+    
     # Calculate error between current quat and desired quat (Wie style)
     def att_error(self):
         qT = np.array(
@@ -305,7 +266,12 @@ targetQuat = targetRot.as_quat()
         #    print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
         #    print(type(event)) 
            
-      
+        t, events = self.ES.find_events(hoboken, t_start, t_end, altitude_degrees=30.0)
+        eventZip = list(zip(t,events))
+        df = pd.DataFrame(eventZip, columns = ["Time", "Event"])
+        culmTimes = df.loc[df["Event"]==2]
+        print(events)
+       
         # Calculate error quaternion
         errorQuat = self.att_error()
         
