2abbd81be43e618d798c9fcdacf4beaf524f6f33
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2025-01-27 17:05:46 -0700

    runtime config organized into credentials, server, simulation

--- nost_tools/config.py
+++ nost_tools/config.py
@@ -76,41 +76,24 @@ class Credentials(BaseModel):
     )
 
 
-# class RuntimeConfig(BaseModel):
-#     username: str = Field(..., description="Username for authentication.")
-#     password: str = Field(..., description="Password for authentication.")
-#     client_id: str = Field(..., description="Client ID for authentication.")
-#     client_secret_key: str = Field(
-#         ..., description="Client secret key for authentication."
-#     )
-#     host: str = Field(..., description="RabbitMQ host.")
-#     rabbitmq_port: int = Field(..., description="RabbitMQ port.")
-#     keycloak_port: int = Field(..., description="Keycloak port.")
-#     keycloak_realm: str = Field(..., description="Keycloak realm.")
-#     virtual_host: str = Field(..., description="RabbitMQ virtual host.")
-#     is_tls: bool = Field(
-#         ..., description="TLS must be enabled for both RabbitMQ and Keycloak."
-#     )
-#     exchanges: Dict[str, Dict] = Field(
-#         default_factory=dict, description="Dictionary of exchanges."
-#     )
-#     channels: List[Dict] = Field(default_factory=list, description="List of channels.")
-#     predefined_exchanges_queues: bool = Field(
-#         False, description="Predefined exchanges and queues."
-#     )
-
-
-class RuntimeConfigUpdate(BaseModel):
-    credentials: Credentials = Field(..., description="Credentials for authentication.")
-    simulation_configuration: Config = (
-        Field(..., description="Simulation configuration."),
-    )
+class SimulationConfig(BaseModel):
     exchanges: Dict[str, Dict] = Field(
         default_factory=dict, description="Dictionary of exchanges."
     )
-    channels: List[Dict] = Field(default_factory=list, description="List of channels.")
+    queues: List[Dict] = Field(default_factory=list, description="List of channels.")
+    execution_parameters: ExecConfig = Field(..., description="Execution parameters.")
     predefined_exchanges_queues: bool = Field(
         False, description="Predefined exchanges and queues."
+    )
+
+
+class RuntimeConfig(BaseModel):
+    credentials: Credentials = Field(..., description="Credentials for authentication.")
+    server_configuration: Config = (
+        Field(..., description="Simulation configuration."),
+    )
+    simulation_configuration: SimulationConfig = Field(
+        ..., description="Simulation configuration."
     )
 
 
@@ -124,116 +107,6 @@ class EnvironmentVariableError(Exception):
 
 class AssertionError(Exception):
     """Assertion error"""
-
-
-def get_exchanges(config: Config) -> Dict[str, Dict]:
-    unique_exchanges = {}
-
-    for app, app_channels in config.channels.items():
-        for channel, details in app_channels.items():
-            bindings = details.get("bindings", {}).get("amqp", {})
-            exchange = bindings.get("exchange", {})
-            exchange_name = exchange.get("name")
-            if exchange_name:
-                exchange_config = ExchangeConfig(
-                    name=exchange_name,
-                    type=exchange.get("type", "topic"),
-                    durable=exchange.get("durable", True),
-                    auto_delete=exchange.get("autoDelete", False),
-                    vhost=exchange.get("vhost", "/"),
-                )
-                if exchange_name in unique_exchanges:
-                    if unique_exchanges[exchange_name] != exchange_config.model_dump():
-                        raise ValueError(
-                            f"Conflicting configurations for exchange '{exchange_name}': {unique_exchanges[exchange_name]} vs {exchange_config.model_dump()}"
-                        )
-                else:
-                    unique_exchanges[exchange_name] = exchange_config.model_dump()
-
-    return unique_exchanges
-
-
-def get_channels(config: Config) -> List[Dict]:
-    channel_configs = []
-
-    for app, app_channels in config.channels.items():
-        for channel, details in app_channels.items():
-            bindings = details.get("bindings", {}).get("amqp", {})
-            exchange = bindings.get("exchange", {})
-            exchange_name = exchange.get("name")
-            address = details.get("address")
-            if address and bindings:
-                channel_config = ChannelConfig(
-                    app=app,
-                    address=address,
-                    exchange=exchange_name or "default_exchange",
-                    durable=exchange.get("durable", True),
-                    auto_delete=exchange.get("autoDelete", False),
-                    vhost=exchange.get("vhost", "/"),
-                )
-                channel_configs.append(channel_config.model_dump())
-
-    return channel_configs
-
-
-def load_env_file(file_path: str) -> Credentials:
-    """
-    Loads an environment (.env) file and returns the parsed data.
-
-    Args:
-        file_path (str): Path to the .env file.
-
-    Returns:
-        Credentials: Parsed environment data.
-    """
-    if not os.path.exists(file_path):
-        raise EnvironmentVariableError("Couldn't load .env file (file not found)")
-
-    load_dotenv(dotenv_path=file_path)
-
-    required_fields = ["USERNAME", "PASSWORD", "CLIENT_ID", "CLIENT_SECRET_KEY"]
-    env_data = {field: os.getenv(field) for field in required_fields}
-
-    missing_fields = [field for field, value in env_data.items() if value is None]
-    if missing_fields:
-        raise EnvironmentVariableError(
-            f"Missing required fields in .env file: {', '.join(missing_fields)}"
-        )
-
-    try:
-        return Credentials(
-            username=env_data["USERNAME"],
-            password=env_data["PASSWORD"],
-            client_id=env_data["CLIENT_ID"],
-            client_secret_key=env_data["CLIENT_SECRET_KEY"],
-        )
-    except ValidationError as err:
-        raise EnvironmentVariableError(f"Invalid environment variables: {err}")
-
-
-def load_yaml_config_file(file_path: str) -> Config:
-    """
-    Loads a YAML configuration file and returns the parsed data.
-
-    Args:
-        file_path (str): Path to the YAML configuration file.
-
-    Returns:
-        Config: Parsed YAML data.
-    """
-    if not os.path.exists(file_path):
-        raise ConfigurationError("Couldn't load config file (not found)")
-
-    with open(file_path, "r", encoding="utf-8") as f:
-        try:
-            yaml_data = yaml.safe_load(f)  # Store the parsed YAML data
-        except yaml.YAMLError as err:
-            raise ConfigurationError(f"Invalid YAML configuration: {err}")
-
-    try:
-        return Config(**yaml_data)
-    except ValidationError as err:
-        raise ConfigurationError(f"Invalid configuration: {err}")
 
 
 class ConnectionConfig:
@@ -289,61 +162,190 @@ class ConnectionConfig:
             env_file (str): Path to the .env file
             yaml_file (str): Path to the YAML configuration file
         """
-        self.yaml_config = None  # Initialize the attribute to store YAML data
-        self.config = None  # Initialize config to None
-        self.yaml_mode = (
-            False  # Initialize the attribute to store the mode of operation
-        )
-        self.predefined_exchanges_queues = False
+        self.yaml_config = None
+        self.yaml_mode = False
 
-        if env_file:
+        self.predefined_exchanges_queues = False
+        self.env_file = env_file
+        self.yaml_file = yaml_file
+        self.unique_exchanges = {}
+        self.channel_configs = []
+
+        self.username = username
+        self.password = password
+        self.host = host
+        self.rabbitmq_port = rabbitmq_port
+        self.keycloak_port = keycloak_port
+        self.keycloak_realm = keycloak_realm
+        self.client_id = client_id
+        self.client_secret_key = client_secret_key
+        self.virtual_host = virtual_host
+        self.is_tls = is_tls
+
+        self.create_connection_config()
+
+    def get_exchanges(self):
+        """
+        Get exchanges from the YAML configuration file.
+        """
+        for app, app_channels in self.yaml_config.channels.items():
+            for channel, details in app_channels.items():
+                bindings = details.get("bindings", {}).get("amqp", {})
+                exchange = bindings.get("exchange", {})
+                exchange_name = exchange.get("name")
+                if exchange_name:
+                    exchange_config = ExchangeConfig(
+                        name=exchange_name,
+                        type=exchange.get("type", "topic"),
+                        durable=exchange.get("durable", True),
+                        auto_delete=exchange.get("autoDelete", False),
+                        vhost=exchange.get("vhost", "/"),
+                    )
+                    if exchange_name in self.unique_exchanges:
+                        if (
+                            self.unique_exchanges[exchange_name]
+                            != exchange_config.model_dump()
+                        ):
+                            raise ValueError(
+                                f"Conflicting configurations for exchange '{exchange_name}': {self.unique_exchanges[exchange_name]} vs {exchange_config.model_dump()}"
+                            )
+                    else:
+                        self.unique_exchanges[exchange_name] = (
+                            exchange_config.model_dump()
+                        )
+
+    def get_channels(self):
+        """
+        Get channels from the YAML configuration file.
+        """
+        for app, app_channels in self.yaml_config.channels.items():
+            for channel, details in app_channels.items():
+                bindings = details.get("bindings", {}).get("amqp", {})
+                exchange = bindings.get("exchange", {})
+                exchange_name = exchange.get("name")
+                address = details.get("address")
+                if address and bindings:
+                    channel_config = ChannelConfig(
+                        app=app,
+                        address=address,
+                        exchange=exchange_name or "default_exchange",
+                        durable=exchange.get("durable", True),
+                        auto_delete=exchange.get("autoDelete", False),
+                        vhost=exchange.get("vhost", "/"),
+                    )
+                    self.channel_configs.append(channel_config.model_dump())
+
+    def get_exchanges_channels(self):
+        """
+        Get exchanges and channels from the YAML configuration file.
+        """
+
+        self.get_exchanges(), self.get_channels()
+        if self.unique_exchanges and self.channel_configs:
+            self.predefined_exchanges_queues = True
+        self.simulation_config = SimulationConfig(
+            exchanges=self.unique_exchanges,
+            queues=self.channel_configs,
+            execution_parameters=self.yaml_config.execution,
+            predefined_exchanges_queues=self.predefined_exchanges_queues,
+        )
+
+    def load_env_file(self) -> Credentials:
+        """
+        Loads an environment (.env) file and returns the parsed data.
+        """
+        if not os.path.exists(self.env_file):
+            raise EnvironmentVariableError("Couldn't load .env file (file not found)")
+
+        load_dotenv(dotenv_path=self.env_file)
+
+        required_fields = ["USERNAME", "PASSWORD", "CLIENT_ID", "CLIENT_SECRET_KEY"]
+        env_data = {field: os.getenv(field) for field in required_fields}
+
+        missing_fields = [field for field, value in env_data.items() if value is None]
+        if missing_fields:
+            raise EnvironmentVariableError(
+                f"Missing required fields in .env file: {', '.join(missing_fields)}"
+            )
+
+        try:
+            return Credentials(
+                username=env_data["USERNAME"],
+                password=env_data["PASSWORD"],
+                client_id=env_data["CLIENT_ID"],
+                client_secret_key=env_data["CLIENT_SECRET_KEY"],
+            )
+        except ValidationError as err:
+            raise EnvironmentVariableError(f"Invalid environment variables: {err}")
+
+    def load_yaml_config_file(self) -> Config:
+        """
+        Loads a YAML configuration file and returns the parsed data.
+        """
+        if not os.path.exists(self.yaml_file):
+            raise ConfigurationError("Couldn't load config file (not found)")
+
+        with open(self.yaml_file, "r", encoding="utf-8") as f:
             try:
-                credentials = load_env_file(env_file)
+                yaml_data = yaml.safe_load(f)  # Store the parsed YAML data
+            except yaml.YAMLError as err:
+                raise ConfigurationError(f"Invalid YAML configuration: {err}")
+
+        try:
+            self.yaml_config = Config(**yaml_data)
+        except ValidationError as err:
+            raise ConfigurationError(f"Invalid configuration: {err}")
+
+    def create_connection_config(self):
+
+        if self.env_file:
+            try:
+                self.credentials_config = self.load_env_file()
             except EnvironmentVariableError as e:
                 raise ValueError(f"Environment variable error: {e}")
         else:
-            credentials = Credentials(
-                username=username,
-                password=password,
-                client_id=client_id,
-                client_secret_key=client_secret_key,
+            self.credentials_config = Credentials(
+                username=self.username,
+                password=self.password,
+                client_id=self.client_id,
+                client_secret_key=self.client_secret_key,
             )
 
-        if yaml_file:
+        if self.yaml_file:
             try:
-                yaml_config = load_yaml_config_file(yaml_file)
+                self.load_yaml_config_file()
             except ConfigurationError as e:
                 raise ValueError(f"Configuration error: {e}")
 
             try:
                 assert all(
-                    item in yaml_config.execution.application_names
-                    for item in yaml_config.channels.keys()
+                    item in self.yaml_config.execution.application_names
+                    for item in self.yaml_config.channels.keys()
                 ), "Application names do not match the channels defined in the configuration file."
             except AssertionError as e:
                 raise ValueError(f"Assertion error: {e}")
         else:
             try:
-                yaml_config = Config(
+                self.yaml_config = Config(
                     servers=ServersConfig(
                         rabbitmq=RabbitMQConfig(
-                            host=host,
-                            port=rabbitmq_port,
-                            virtual_host=virtual_host,
-                            tls=is_tls,
+                            host=self.host,
+                            port=self.rabbitmq_port,
+                            virtual_host=self.virtual_host,
+                            tls=self.is_tls,
                         ),
                         keycloak=KeycloakConfig(
-                            host=host,
-                            port=keycloak_port,
-                            realm=keycloak_realm,
-                            tls=is_tls,
+                            host=self.host,
+                            port=self.keycloak_port,
+                            realm=self.keycloak_realm,
+                            tls=self.is_tls,
                         ),
                     ),
                     channels={},
                     execution=ExecConfig(),
                 )
             except:
-                yaml_config = Config(
+                self.yaml_config = Config(
                     servers=ServersConfig(
                         rabbitmq=RabbitMQConfig(), keycloak=KeycloakConfig()
                     ),
@@ -351,37 +353,18 @@ class ConnectionConfig:
                     execution=ExecConfig(),
                 )
 
-        unique_exchanges, channel_configs = get_exchanges(yaml_config), get_channels(
-            yaml_config
-        )
+        self.get_exchanges_channels()
 
-        if unique_exchanges and channel_configs:
-            self.predefined_exchanges_queues = True
-            self.config = yaml_config
+        server_config = self.yaml_config.copy()
+        if hasattr(server_config, "channels"):
+            del server_config.channels
+        if hasattr(server_config, "execution"):
+            del server_config.execution
 
-        # self.rc = RuntimeConfig(
-        #     username=credentials.username,
-        #     password=credentials.password,
-        #     client_id=credentials.client_id,
-        #     client_secret_key=credentials.client_secret_key,
-        #     host=yaml_config.servers.rabbitmq.host,
-        #     rabbitmq_port=yaml_config.servers.rabbitmq.port,
-        #     keycloak_port=yaml_config.servers.keycloak.port,
-        #     keycloak_realm=yaml_config.servers.keycloak.realm,
-        #     virtual_host=yaml_config.servers.rabbitmq.virtual_host,
-        #     is_tls=yaml_config.servers.rabbitmq.tls
-        #     and yaml_config.servers.keycloak.tls,
-        #     exchanges=unique_exchanges,
-        #     channels=channel_configs,
-        #     predefined_exchanges_queues=self.predefined_exchanges_queues,
-        # )
-
-        self.rc = RuntimeConfigUpdate(
-            credentials=credentials,
-            simulation_configuration=yaml_config,
-            exchanges=unique_exchanges,
-            channels=channel_configs,
-            predefined_exchanges_queues=self.predefined_exchanges_queues,
+        self.rc = RuntimeConfig(
+            credentials=self.credentials_config,
+            server_configuration=server_config,
+            simulation_configuration=self.simulation_config,
         )
 
 
@@ -394,9 +377,9 @@ def create_connection_config(
     keycloak_realm: Optional[str] = None,
     virtual_host: Optional[str] = None,
     is_tls: bool = None,
-    env_file: Optional[str] = None,
     client_id: Optional[str] = None,
     client_secret_key: Optional[str] = None,
+    env_file: Optional[str] = None,
     yaml_file: Optional[str] = None,
 ) -> ConnectionConfig:
     """
@@ -411,9 +394,9 @@ def create_connection_config(
         keycloak_realm (Optional[str]): Keycloak realm.
         virtual_host (Optional[str]): Virtual host.
         is_tls (bool): Whether to use TLS. Defaults to True.
-        env_file (Optional[str]): Path to the environment file.
         client_id (Optional[str]): Client ID.
         client_secret_key (Optional[str]): Client secret key.
+        env_file (Optional[str]): Path to the environment file.
         yaml_file (Optional[str]): Path to the YAML configuration file.
 
     Returns:
